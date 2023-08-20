60f024cbd7d5a2d6b3edf114bec36652ec536cc4
Kaue Miziara <kauemiziara@gmail.com>
2023-08-19 23:00:26 -0300

    Add docstrings to Model class

--- src/models/qr_code_model.py
+++ src/models/qr_code_model.py
@@ -1,7 +1,16 @@
 from kivymd.uix.snackbar import Snackbar
 
 class QRCodeModel:
+    """Handles QR code data and related logic."""
+
     def handle_symbols(self, instance, symbols):
+        """
+        Handle QR code symbols and display related information using a Snackbar.
+
+        Parameters:
+            instance: The instance of the QR code widget that triggered the event.
+            symbols (list): List of QR code symbols detected.
+        """
         if not symbols == "":
             for symbol in symbols:
                 data = symbol.data.decode()
